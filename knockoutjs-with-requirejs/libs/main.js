f1f66cf181fd2c20a9ea64e10f2718113ff1ea59
jes <samueljesro@gmail.com>
2018-08-19 12:50:50 +0530

    Create main.js

---
+++ knockoutjs-with-requirejs/libs/main.js
@@ -0,0 +1,26 @@
+require.config({
+	baseUrl:'libs',
+	paths:{
+		'jquery':[
+		'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min',
+		'jquery-3.3.1.min'
+		],
+		'knockoutjs':[
+		'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min',
+		'knockout-3.4.2'
+		],
+		'bootstrap': 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min',
+		'custom':'../modules/custom',
+		'paginate':'../modules/paginate',
+		'work':'../modules/work',
+		'workone':'../modules/workone'
+	},
+	shim:{
+		'jquery':{
+			exports:'$'
+		},
+		'bootstrap':{
+			deps: ['jquery']
+		}
+	}
+});
