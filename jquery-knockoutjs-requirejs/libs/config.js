3b830691c9a10ec65298f017d920c2988c8f480e
jes <samueljesro@gmail.com>
2018-08-20 16:41:02 +0530

    Create config.js

---
+++ jquery-knockoutjs-requirejs/libs/config.js
@@ -0,0 +1,25 @@
+require.config({
+	baseUrl:'libs',
+	paths:{
+		'jquery':[
+		'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min',
+		'jquery-3.3.1.min'
+		],
+		'knockoutjs':[
+		'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min',
+		'knockout-3.4.2'
+		],
+		'bootstrap': 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min',
+		'main':'../modules/main',
+		'product':'../modules/product',
+		'slick':'slick.min'
+	},
+	shim:{
+		'jquery':{
+			exports:'$'
+		},
+		'bootstrap':{
+			deps: ['jquery']
+		}
+	}
+});
